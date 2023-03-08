04b50af93c4f9827b0d0e06f7e08a5cc12b870b3
Charanvir <charanvir123@gmail.com>
2023-03-07 19:30:50 -0500

    created class to display form elements and methods to access value and eventListeners

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,68 @@
-// Code goes here!
+// autobind decorator
+function autoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+// ProjectInput Class
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputEl: HTMLInputElement;
+  descriptionInputEl: HTMLInputElement;
+  peopleInputEl: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    // creates a copy of a document fragment
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    // We can specify this because we know that the first element is a form element
+    this.element = importedNode.firstElementChild! as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputEl = this.element.querySelector(
+      "#title"
+    )! as HTMLInputElement;
+    this.descriptionInputEl = this.element.querySelector(
+      "#description"
+    )! as HTMLInputElement;
+    this.peopleInputEl = this.element.querySelector(
+      "#people"
+    )! as HTMLInputElement;
+
+    this.attach();
+    this.configure();
+  }
+
+  @autoBind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputEl.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler);
+  }
+
+  private attach() {
+    // inserted after the beginning tag of the element that is passed through
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
